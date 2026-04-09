21a26cb86ef820948cc5801fbedbc8295c397fb2
AnLeHoai <anle31903@gmail.com>
2026-04-09 22:28:10 +0700

    Add project files.

---
+++ FinalProject__SaigonRide/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using FinalProject__SaigonRide.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace FinalProject__SaigonRide.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
