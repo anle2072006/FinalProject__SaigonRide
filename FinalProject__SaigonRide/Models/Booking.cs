85237b6066abf6551bf7b92fb5bc9c5a30fe2ed3
AnLeHoai <anle31903@gmail.com>
2026-04-10 23:37:11 +0700

    Add models and database setup

---
+++ FinalProject__SaigonRide/Models/Booking.cs
@@ -0,0 +1,15 @@
+﻿namespace FinalProject__SaigonRide.Models
+{
+    public class Booking
+    {
+        public int Id { get; set; }
+
+        public string UserId { get; set; } = "";
+        public int VehicleId { get; set; }
+
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+
+        public double TotalPrice { get; set; }
+    }
+}
---
+++ FinalProject__SaigonRide/Models/Stations.cs
@@ -0,0 +1,9 @@
+﻿namespace FinalProject__SaigonRide.Models
+{
+    public class Station
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+        public string Location { get; set; } = "";
+    }
+}
