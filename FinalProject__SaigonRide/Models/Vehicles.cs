4bc4ed7e204b327ed44380290e69b380f5bb08d9
AnLeHoai <anle31903@gmail.com>
2026-05-01 14:09:41 +0700

    fefw

--- FinalProject__SaigonRide/Models/Vehicles.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FinalProject__SaigonRide.Models
-{
-    public class Vehicle
-    {
-        public int Id { get; set; }
-        public string Name { get; set; } = " ";
-        public double PricePerHour { get; set; }
-    }
-}
