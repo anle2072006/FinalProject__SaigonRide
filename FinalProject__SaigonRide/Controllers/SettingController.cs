0a794fc7f0c3e3bb982862c23f435c3a133645e6
AnLeHoai <anle31903@gmail.com>
2026-05-04 09:39:31 +0700

    controller

---
+++ FinalProject__SaigonRide/Controllers/SettingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FinalProject__SaigonRide.Controllers
+{
+    public class SettingController : Controller
+    {
+        public IActionResult IndexSetting()
+        {
+            return View();
+        }
+    }
+}
