4ad4dd7af9917ed49e93bd1f74e462f0a350f3fa
AnLeHoai <anle31903@gmail.com>
2026-05-04 22:40:49 +0700

    InUseControler

--- FinalProject__SaigonRide/Controllers/InUseController.cs
+++ FinalProject__SaigonRide/Controllers/InUseController.cs
@@ -1,6 +1,12 @@
-﻿namespace FinalProject__SaigonRide.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FinalProject__SaigonRide.Controllers
 {
-    public class InUseController
+    public class InUseController : Controller
     {
+        public IActionResult IndexInUse()
+        {
+            return View();
+        }
     }
 }
